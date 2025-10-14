318a9f567a9633e4e90f0375f60feefd641e4251
RichardRodri26 <richardmanuel811@gmail.com>
2025-10-13 21:09:36 -0500

    feat: configure NATS transport and update environment variables for NATS server

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,15 +4,22 @@ import * as joi from 'joi';
 export interface EnvVars {
   PORT: number;
   DATABASE_URL: string;
+
+  NATS_SERVERS: string[];
 }
 
 const envsSchema = joi.object<EnvVars>({
   PORT: joi.number().required(),
   DATABASE_URL: joi.string().required(),
+
+  NATS_SERVERS: joi.array().items(joi.string()).required(),
 })
   .unknown(true);
 
-const { error, value } = envsSchema.validate(process.env);
+const { error, value } = envsSchema.validate({
+  ...process.env,
+  NATS_SERVERS: process.env.NATS_SERVERS?.split(',') || [],
+});
 
 if (error) {
   throw new Error(`Config validation error: ${error.message}`);
@@ -25,4 +32,5 @@ const envVars = value;
 export const envs = {
   port: envVars.PORT,
   databaseUrl: envVars.DATABASE_URL,
+  natsServers: envVars.NATS_SERVERS,
 }
